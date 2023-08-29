8346255b7b591da4167d7d002140c3674fd511d3
Tyler Bergstrom <tbergstrom05@gmail.com>
2023-08-29 15:40:35 -0700

    completed project 1

--- section-1/hello.py
+++ section-1/hello.py
@@ -10,8 +10,8 @@ print("New lines can be created with a backslash and n.")
 
 
 print("3" * 50)  # 3: Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
-inputName = input("What is your name?")
-print(len(inputName))
+input_name = input("What is your name?")
+print(len(input_name))
 
 
 print("4" * 50)  # 4: Write a program that switches the values stored in the variables a and b.
@@ -25,3 +25,19 @@ b = temp
 print("a: " + a)
 print("b: " + b)
 
+
+print("5" * 25, " Day 1 Project ", "5" * 25)
+# Project :#1. Create a greeting for your program.
+# 2. Ask the user for the city that they grew up in.
+#
+# 3. Ask the user for the name of a pet.
+#
+# 4. Combine the name of their city and pet and show them their band name.
+#
+# 5. Make sure the input cursor shows on a new line:
+
+print("Welcome to Band Name Generator")
+city_input = input("Please enter the name of your city: \n")
+pet_input = input("Please enter your pet's name: \n")
+band_name = city_input + " " + pet_input
+print("Your band name could be " + band_name)

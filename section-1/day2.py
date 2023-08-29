dcf4aac0a5650dfe3ab205f6e8b3c37a468df278
Tyler Bergstrom <tbergstrom05@gmail.com>
2023-08-29 16:12:32 -0700

    Started day2, renamed hello.py -> day1.py

---
+++ section-1/day2.py
@@ -0,0 +1,12 @@
+# 1
+print("1" * 50)  # Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+input_number = input("Gimme a two digit number NOW: \n")
+first_digit = input_number[0]
+second_digit = input_number[1]
+sum_digits = int(second_digit) + int(first_digit)
+
+print(sum_digits)
+
+
+# 2
+print("2" * 50)  # 

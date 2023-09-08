f45beafb90c099cd8284b84ec27a354e5182b6e8
Tyler Bergstrom <tbergstrom05@gmail.com>
2023-09-08 12:29:34 -0700

    started day 3

---
+++ section-3/day3.py
@@ -0,0 +1,58 @@
+####################################### BMI 2.0 #######################################
+
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+#
+# It should tell them the interpretation of their BMI based on the BMI value.
+#
+#     Under 18.5 they are underweight
+#     Over 18.5 but below 25 they have a normal weight
+#     Over 25 but below 30 they are slightly overweight
+#     Over 30 but below 35 they are obese
+#     Above 35 they are clinically obese.
+
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+bmi = round(weight/ height ** 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight")
+elif 18.5 < bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight")
+elif 25 < bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight")
+elif 30 < bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese")
+
+
+####################################### Leap Year #######################################
+
+# Write a program that works out whether if a given year is a leap year.
+
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+# A leap year must be:
+
+# Evenly divisible by 4
+# NOT evenly divisible by 100
+# Evenly divisible by 400
+
+if year % 4 == 0:
+    if year % 100 != 0 or year % 400 == 0:
+        print("Leap year.")
+    else:
+        print("Not leap year.")
+else:
+    print("Not leap year.")
